108d73513db2e26ad86a0c93d866a5da05866385
rob <un@rob.mx>
2020-07-12 21:02:45 -0400

    vault + dsm fuckery

--- ansible/export-config.py
+++ ansible/export-config.py
@@ -2,44 +2,99 @@ import config_yourself as cy
 from json import dumps as json_encode
 from os import environ
 from base64 import b64encode
+from sys import argv
+
+def is_primitive(el): return isinstance(el, (str, int, float))
+
+class ConfigProcessor(object):
+    def __init__(self, data, path):
+        self._data = []
+        self.process(data, path)
+
+    def dict(self, data, path):
+        pass
+
+    def list(self, data, path):
+        pass
+
+    def primitive(self, data, path):
+        pass
+
+    def write(self, name):
+        raise Exception(f"Unknown formatter {name}")
+
+    def process(self, data, path):
+        if isinstance(data, dict):
+            self.dict(data, path)
+        elif isinstance(data, list):
+            self.list(data, path)
+        else:
+            self.primitive(data, path)
+
+class Consul(ConfigProcessor):
+    def primitive(self, data, path, raw=False):
+        if raw:
+            path += ["_json"]
+            data = json_encode(data)
+
+        self._data.append({
+            'key': '/'.join(path),
+            'flags': 0,
+            'value': b64encode(str(data).encode('utf-8')).decode('utf-8'),
+        })
+
+    def list(self, data, path):
+        if len(data) == 0 or all(map(is_primitive, data)):
+            return self.primitive(data, path, raw=True)
+
+        for k, v in enumerate(data):
+            self.process(v, path + [str(k)])
+
+    def dict(self, data, path):
+        for k, v in data.items():
+            if str(k)[0] == '_': pass
+            self.process(v, path + [k])
+
+    def write(self):
+        return json_encode(self._data)
+
+
+class Vault(ConfigProcessor):
+    def __init__(self, data, path):
+        self._data = {}
+        self.process(data, path)
+
+    def primitive(self, data, path, raw=False):
+        self._data['/'.join(path)] = {"json": json_encode(data)} if raw else data
+
+    def list(self, data, path):
+        if len(data) == 0 or all(map(is_primitive, data)):
+            return self.primitive(data, path, raw=True)
+        else:
+             for k, v in enumerate(data):
+                self.process(v, path + [str(k)])
+
+    def dict(self, data, path):
+        top = dict()
+        for k, v in data.items():
+            if str(k)[0] == '_': pass
+            elif not is_primitive(v): self.process(v, path + [str(k)])
+            else: top[k] = v
+
+        if len(top.keys()) > 0:
+            self.primitive(top, path)
+
+    def write(self):
+        return "\n".join([
+            f"{k} {json_encode(v)}" for k, v in self._data.items()
+        ])
 
 file = cy.load.file(environ.get('CONFIG_FILE', 'config.yml'))
 password = environ.get('CONFIG_PASSWORD')
-
 data = dict(cy.Config(file, password = password))
 del data['crypto']
 
-def recurse (col, d, path):
-    if isinstance(d, dict):
-        for k, v in d.items():
-            if str(k)[0] == '_':
-                continue
+data['nodes']['json'] = json_encode(data['nodes'])
 
-            subpath = path + [str(k)]
-            recurse(col, v, subpath)
-    elif isinstance(d, list):
-        if len(d) == 0 or isinstance(d[0], str):
-            # add a copy of json-encoded data
-            # useful for interpolation in toml configs and stuff
-            col.append({
-                'key': '/'.join(path + ["_json"]),
-                'flags': 0,
-                'value': b64encode(json_encode(d).encode('utf-8')).decode('utf-8'),
-            })
-        for k, v in enumerate(d):
-            subpath = path + [str(k)]
-            recurse(col, v, subpath)
-    else:
-        col.append({
-            'key': '/'.join(path),
-            'flags': 0,
-            'value': b64encode(str(d).encode('utf-8')).decode('utf-8'),
-        })
-
-
-    return col
-
-values = []
-
-print(json_encode(recurse(values, data, ['nidito', 'config'])))
-# consul kv import <(pipenv run python config-to-consul.py)
+processor = Consul if argv[1:] == ["consul"] else Vault
+print(processor(data, ['nidito', 'config']).write())
