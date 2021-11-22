0cb1d3af09e046e1aca1665c721dd5fc23f35b64
Roberto Hidalgo <un@rob.mx>
2021-11-22 01:02:48 -0600

    moar milpa

--- .milpa/commands/config/get.sh
+++ .milpa/commands/config/get.sh
@@ -5,7 +5,7 @@ case "$MILPA_OPT_SOURCE" in
     vault kv get "nidito/config/$MILPA_ARG_FILE/${MILPA_ARG_PATH//.//}"
   ;;
   local)
-    gcy get "$CONFIG_DIR/$MILPA_ARG_FILE.yaml" "${MILPA_ARG_PATH:- .}"
+    gcy get "$CONFIG_DIR/$MILPA_ARG_FILE.yaml" "${MILPA_ARG_PATH:-.}"
   ;;
   *) @milpa.fail "Unknown source $MILPA_OPT_SOURCE"
 esac
--- .milpa/hooks/before-run.sh
+++ .milpa/hooks/before-run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-NIDITO_ROOT="$(dirname "$MILPA_COMMAND_REPO")"
+NIDITO_ROOT="$(dirname "$(readlink "$MILPA_COMMAND_REPO")")"
 export CONSUL_HTTP_ADDR="https://consul.service.consul:5554"
 export NOMAD_ADDR="https://nomad.service.consul:5560"
 export VAULT_ADDR="${VAULT_ADDR:-https://vault.service.consul:5570}"
-export CONFIG_FILE="$NIDITO_ROOT/config.yml"
+export CONFIG_DIR="$NIDITO_ROOT/config"
 
 @milpa.load_util "config"
