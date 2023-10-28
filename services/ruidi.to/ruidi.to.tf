dcc141115ff4bda4a83177412bac4bec0134d8d6
Roberto Hidalgo <un@rob.mx>
2023-10-28 00:26:34 -0600

    play with garage terraform provider

--- services/ruidi.to/ruidi.to.tf
+++ services/ruidi.to/ruidi.to.tf
@@ -16,9 +16,23 @@ terraform {
       source  = "hashicorp/vault"
       version = "~> 3.18.0"
     }
+    garage = {
+      source = "nidito/garage"
+      version = "0.0.1"
+    }
   }
 
   required_version = ">= 1.0.0"
+}
+
+provider "garage" {
+  host = "api.garage.nidi.to"
+  scheme = "https"
+  token = jsondecode(data.vault_generic_secret.garage.data.token).admin
+}
+
+data "vault_generic_secret" "garage" {
+  path = "cfg/svc/tree/nidi.to:garage"
 }
 
 provider "digitalocean" {
@@ -52,4 +66,21 @@ resource "consul_keys" "cdn-config" {
     path = "cdn/ruidi.to"
     value = "ruidi.to"
   }
+}
+
+
+resource "garage_bucket" "ruiditos" {
+  website_access_enabled = true
+  website_config_error_document = "error.html"
+  website_config_index_document = "index.html"
+}
+
+resource "garage_bucket_global_alias" "ruiditos_alias" {
+  bucket_id = garage_bucket.ruiditos.id
+  alias     = "ruiditos"
+}
+
+output "bucket" {
+  value = garage_bucket.ruiditos.id
+  description = "the bucket id"
 }
